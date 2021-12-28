b90127dc99c1fa326c6a1d98e94808cfa2dc30bf
lucasForato <lucas7forato@gmail.com>
2021-12-27 21:51:51 -0300

    added frontend implementation home

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 /** importing our pages */
 import Tracks from './tracks';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache(),
+});
+
 export default function Pages() {
   return (
-    <Router primary={false} component={Fragment}>
-      <Tracks path="/" />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router primary={false} component={Fragment}>
+        <Tracks path="/" />
+      </Router>
+    </ApolloProvider>
   );
 }
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+export const TRACKS = gql`
+  query getTracks {
+    getTracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+
+  return <Layout grid>
+  {data.getTracksForHome.map(track => (
+    <TrackCard key={track.id} track={track} />
+  ))}
+</Layout>;
 };
 
 export default Tracks;
